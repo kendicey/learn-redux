3444b787e7bd30071e4fa91b01b4db4b0b229d78
kendice <kendiceyeungnm@gmail.com>
2023-06-10 15:10:31 -0700

    1. Basics

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux';
+import './App.css';
+import {actions} from './store';
+
+function App() {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  }
+  const decrement = () => {
+    dispatch(actions.decrement());
+  }
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  }
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={addBy}>Add</button>
+    </div>
+  );
+}
+
+export default App;
